ae418ab8c1d3f13517c938531ecdd78303b70144
Maximiliano Contreras <100421513+pablomcontreras@users.noreply.github.com>
2022-06-02 15:44:33 -0300

    Avanzado hasta 1.5

---
+++ part1/1/src/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
+const Header = (props) => {
+
+  return (
+    <div>
+      <h1>{props.course.name}</h1>
+      </div>)
+}
+const Part = (props) =>{
+
+return(
+    <div>
+    <p>
+      {props.part} {props.exercise}
+    </p>
+</div>
+)
+
+}
+
+const Content = (props) => {
+
+  return (
+    <div>
+      <Part
+        part={props.parts.parts[0].name}
+        exercise={props.parts.parts[0].exercises}
+      />
+          <Part
+        part={props.parts.parts[1].name}
+        exercise={props.parts.parts[1].exercises}
+      />
+          <Part
+        part={props.parts.parts[2].name}
+        exercise={props.parts.parts[2].exercises}
+      />
+     
+    </div>
+  )
+  
+};
+
+const Total = (props) => {
+
+  return (
+    <div>
+    <p>Number of exercises {props.parts.parts[0].exercises + props.parts.parts[1].exercises + props.parts.parts[2].exercises} </p>
+  </div>
+  )
+}
+
+const App = (props) => {
+
+const course = 
+{
+  name: 'Half Stack application development',
+  parts: [
+  {
+  name: 'Fundamentals of React',
+  exercises: 10
+},
+{
+  name: 'Using props to pass data',
+  exercises: 7
+},
+{
+  name: 'State of a component',
+  exercises: 14
+}
+]
+}
+  return (
+    <div>
+    <Header course={course}  />
+    <Content parts = {course} />
+    <Total parts = {course} />
+  </div>
+
+  )
+}
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
